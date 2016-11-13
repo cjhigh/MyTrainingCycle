5ac33de0541bbb5ad518504786092b4dff8f399c
cjhigh <cjhigh@gmail.com>
2016-11-13 09:13:36 -0500

    Just enough for a test run

--- Scripts/mtc.js
+++ Scripts/mtc.js
@@ -1,2 +1,13 @@
-var s = "hello";
-alert(s);
+<script
+  src="https://code.jquery.com/jquery-3.1.1.min.js"
+  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+  crossorigin="anonymous"></script>
+
+var authState = null;
+function load()
+{
+	if(authState == null)
+	{
+		var
+		}
+	}//function load()
