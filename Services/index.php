d177bd3e5ae46941393ded7a732aa65890170e1d
cjhigh <cjhigh@gmail.com>
2016-11-14 19:12:35 -0500

    A little reorganization

--- Services/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-//echo $_GET['code'];
-if($_GET['code'] != null)
-{
-	setcookie("stravaCode", $_GET['code']);
-	}
-?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+include '../vendor/autoload.php';
+use Strava\API\OAuth;
+use Strava\API\Exception;
+
+try {
+    $options = array(
+        'clientId'     => 14614,
+        'clientSecret' => 'APP-TOKEN',
+        'redirectUri'  => 'http://localhost/callback.php'
+    );
+    $oauth = new OAuth($options);
+
+    if (!isset($_GET['code'])) {
+        print '<a href="'.$oauth->getAuthorizationUrl().'">connect</a>';
+    } else {
+        $token = $oauth->getAccessToken('authorization_code', array(
+            'code' => $_GET['code']
+        ));
+        print $token;
+    }
+} catch(Exception $e) {
+    print $e->getMessage();
+}
+?>
